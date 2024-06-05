c6afb2698f4473d6c5409456b51aeb742fd523c8
Volodymyr <zmiyov88@gmail.com>
2024-06-05 09:58:55 +0300

    Initial Commit

---
+++ UniverseOnboardingTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniverseOnboardingTest
+//
+//  Created by Vladimir Pisarenko on 05.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
